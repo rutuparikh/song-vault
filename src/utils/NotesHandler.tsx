a44355be515918c51c0ba6ec2ca41a6380f00da0
rutuparikh <rutupatel62@gmail.com>
2025-05-09 13:26:59 -0700

    Updates

--- src/utils/NotesHandler.tsx
+++
@@ -1,9 +0,0 @@
-import { ChangeEvent } from 'react';
-
-const handleNotesChange = (event: ChangeEvent<HTMLTextAreaElement>,
-    setNotes: (value: string) => void) => {
-
-    const value = event.target.value;
-    setNotes(value);
-};
-export default handleNotesChange;
